ccdadcb583673145ef2da45d2281a454253e8a94
Kabir Nitin Birari <86940182+kabir2112@users.noreply.github.com>
2024-08-04 10:00:16 +0000

    done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"user strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are using NodeJs, not Browser
+
+console.log(3 
+	+ 
+	3); // code readablitity shold be high
+ 
+console.log("kabir");
+
+let name = "kabir"
+let age = 24
+let isLoggedIn = true
+
+// number => 2 to power of 53
+// bigint
+// string ""
+// boolean = true/false
+// null => standalone value
+// undefined => value is not assign yet
+// symbol => unique
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
