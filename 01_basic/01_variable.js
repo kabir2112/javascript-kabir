1e7661dfcbc43f837ca8cafb9860f6642c0e22b0
Kabir Nitin Birari <86940182+kabir2112@users.noreply.github.com>
2024-08-04 09:59:38 +0000

    done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 34567
+let accountEmail ="kabir.birari@gmail.com"
+var accountPassword="1234"
+
+accountCity = "Pune"
+let accountState;
+
+//accountId = 2
+accountEmail = "harshal.kk@gmail.com"
+accountCity = "Mumbai"
+accountPassword = " 12344221"
+/* Prefer not use to Var
+because of issues in block scope aand finctional scope 
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
