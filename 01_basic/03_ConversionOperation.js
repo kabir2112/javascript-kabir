bec10f0cd3b8e21edc5379b379701787112f8c5b
Kabir Nitin Birari <86940182+kabir2112@users.noreply.github.com>
2024-08-05 04:29:01 +0000

    Conversion Operation

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+null => 0
+undefined => NaN
+true = 1
+"kabir" = NaN
+ */
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true || 0 = false
+// "kabir" = true || "" = false
+
+let someNumber = 33
+let IsStringIn = String(someNumber)
+console.log(IsStringIn);
+console.log(typeof IsStringIn);
+
